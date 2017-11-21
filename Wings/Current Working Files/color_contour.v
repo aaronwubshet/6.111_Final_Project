9dd189a75c5bfae999f7f6b7ddf73540e415141f
yeungw <yeungw@mit.edu>
2017-11-21 03:09:29 -0500

    separates edges into sections, but stops halfway

--- Wings/Current Working Files/color_contour.v
+++ Wings/Current Working Files/color_contour.v
@@ -18,8 +18,7 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-
-
+    
 module color_contour(
     input clk,
     input [9:0] x_start,
@@ -31,41 +30,25 @@ module color_contour(
     output reg done = 0,
     
     output reg [18:0] addr,
-    output reg [2:0] edge_out,
+    input [2:0] edge_out,
     output reg [2:0] bin_in,
     output reg we,
     
-    output reg [1:0] state_out,
+    output reg [2:0] state_out,
     input start,
     output reg [11:0] pixel_count,
     output reg [2:0] set_bin_out
     );
-    
-    wire [23:0] div_out;
-    reg [11:0] pixel_per_bin;
-    wire div_ready;
-    div_gen_0 pixel_per(.s_axis_divisor_tdata(num_pixels), .s_axis_divisor_tvalid(1), .s_axis_dividend_tdata(num_bins), 
-    .s_axis_dividend_tvalid(1), .aclk (clk), .m_axis_dout_tdata(div_out), .m_axis_dout_tvalid(div_ready));
-    
-    
-//    reg [11:0] pixel_count;
-    reg [2:0] bin = 1;
-    
-//    //reading bram
-//    reg [18:0]addr = 0;
-//    reg [2:0] edge_out;
-//    reg [2:0] bin_in;
-//    reg en = 0;
-//    reg we = 0;    //0 for read, 1 for write
-              
-//    xy_bin get_if_edge(.addra(addr), .clka(clk), .dina(bin_in), .douta(edge_out), .ena(en), .wea(we));
-    
+        
     parameter STATE_SETUP = 0;
     parameter STATE_WAIT = 1;
     parameter STATE_EXPLORE = 2;
     parameter STATE_IS_IT_EDGE = 3;
+    parameter STATE_WAIT_TWO = 4;
+    parameter STATE_DONE = 5;
     
     reg [2:0] explore_dir = 0;
+    reg [2:0] explore_dir_count = 0;
     reg [2:0] max_explore_dir = 3'b111;
     parameter DIR_GET_RIGHT = 0;
     parameter DIR_GET_DOWNRIGHT = 1;
@@ -76,8 +59,8 @@ module color_contour(
     parameter DIR_GET_UP = 6;
     parameter DIR_GET_UPRIGHT = 7;
     
-    reg [1:0] state = STATE_SETUP;
-    reg [1:0] next_state;
+    reg [2:0] state = STATE_SETUP;
+    reg [2:0] next_state;
     
     reg [9:0] x_prev = 0;
     reg [8:0] y_prev = 0;
@@ -85,44 +68,41 @@ module color_contour(
     reg [9:0] x_curr;
     reg [8:0] y_curr;
     reg [18:0] addr_curr = 0;
-    
-    reg [2:0] set_bin;
-    
+        
     reg [9:0] x_explore;
     reg [8:0] y_explore;
     reg [18:0] addr_explore = 0;
     
-    always @(posedge clk) begin
-//        if (reset) begin
-//            done <= 0;
-//            state <= STATE_SETUP;
-//        end
-        
-//        else begin
+    reg [11:0] pixel_per_bin; 
+    
+    
+  always @(posedge clk) begin
             state_out <= state;
-            if (start && ~done) begin
-//            pixel_per_bin[11:0] <= div_out[19:8];
+            set_bin_out <= bin_in;
             
+            if (start) begin            
             pixel_per_bin <= 9'b101011100;
-            set_bin_out <= set_bin;
                         
             case (state)
                 STATE_SETUP: begin
-                    set_bin <= 2;
+                    bin_in <= 1;
                     done <= 0;
                     addr <= addr_start;
-                    x_curr <= x_start;
-                    y_curr <= y_start;
                     addr_curr <= addr_start; 
                     
                     pixel_count <= 0;
                     
                     next_state <= STATE_EXPLORE;
                     state <= STATE_WAIT;
+                    
+                    we <= 1;
                 end
                 
                 STATE_WAIT: begin
-                    //waste 1 clock cycle
+                    state <= STATE_WAIT_TWO;
+                end
+                
+                STATE_WAIT_TWO: begin
                     state <= next_state;
                 end
                 
@@ -134,148 +114,118 @@ module color_contour(
                     
                     case (explore_dir)
                         DIR_GET_RIGHT: begin                        
-                            if (addr + 1 == addr_prev) begin
+                            if (addr_curr + 1 == addr_prev) begin
                                 state <= STATE_EXPLORE;
+                                explore_dir <= explore_dir + 1;
                             end else begin
-                                x_explore <= x_curr + 1;
-                                y_explore <= y_curr;
-                                addr_explore <= addr + 1;
-
+                                addr <= addr_curr + 1;
                             end
-                            
-                            explore_dir <= DIR_GET_DOWNRIGHT;
-                            
-    
                         end
                         
                         DIR_GET_DOWNRIGHT: begin
-                            if (addr + 640 + 1 == addr_prev) begin
+                            if (addr_curr + 640 + 1 == addr_prev) begin
                                 state <= STATE_EXPLORE;
+                                explore_dir <= explore_dir + 1;
                             end else begin                                                        
-                                x_explore <= x_curr + 1;
-                                y_explore <= y_curr + 1;
-                                addr_explore <= addr + 640 + 1;
+                                addr <= addr_curr + 640 + 1;
                             end
-                            
-                            explore_dir <= DIR_GET_DOWN;
                         end
                         
                         DIR_GET_DOWN: begin
-                            if (addr + 640 == addr_prev) begin
+                            if (addr_curr + 640 == addr_prev) begin
                                 state <= STATE_EXPLORE;
+                                explore_dir <= explore_dir + 1;
                             end else begin
-                                x_explore <= x_curr;
-                                y_explore <= y_curr + 1;
-                                addr_explore <= addr + 640;
+                                addr <= addr_curr + 640;
                             end
-                            
-                            explore_dir <= DIR_GET_DOWNLEFT;                        
                         end
                         
                         DIR_GET_DOWNLEFT: begin
-                            if (addr + 640 - 1 == addr_prev) begin
+                            if (addr_curr + 640 - 1 == addr_prev) begin
                                 state <= STATE_EXPLORE;
+                                explore_dir <= explore_dir + 1;
                             end else begin
-                                x_explore <= x_curr - 1;
-                                y_explore <= y_curr + 1;
-                                addr_explore <= addr + 640 - 1;
+                                addr <= addr_curr + 640 - 1;
                             end
-                            
-                            explore_dir <= DIR_GET_LEFT;
                         end
                         
                         DIR_GET_LEFT: begin
-                            if (addr - 1 == addr_prev) begin
+                            if (addr_curr - 1 == addr_prev) begin
                                 state <= STATE_EXPLORE;
+                                explore_dir <= explore_dir + 1;
                             end else begin
-                                x_explore <= x_curr - 1;
-                                y_explore <= y_curr;
-                                addr_explore <= addr - 1;
+                                addr <= addr_curr - 1;
                             end
-                            
-                            explore_dir <= DIR_GET_UPLEFT;
                         end
                         
                         DIR_GET_UPLEFT: begin
-                            if (addr - 640 - 1 == addr_prev) begin
+                            if (addr_curr - 640 - 1 == addr_prev) begin
                                 state <= STATE_EXPLORE;
+                                explore_dir <= explore_dir + 1;
                             end else begin
-                                x_explore <= x_curr - 1;
-                                y_explore <= y_curr - 1;
-                                addr_explore <= addr - 640 - 1;
+                                addr <= addr_curr - 640 - 1;
                             end
-                            
-                            explore_dir <= DIR_GET_UP;
                         end
                         
                         DIR_GET_UP: begin
-                            if (addr - 640 == addr_prev) begin
+                            if (addr_curr - 640 == addr_prev) begin
                                 state <= STATE_EXPLORE;
+                                explore_dir <= explore_dir + 1;
                             end else begin
-                                x_explore <= x_curr;
-                                y_explore <= y_curr - 1;
-                                addr_explore <= addr - 640;
+                                addr_curr <= addr_curr - 640;
                             end
-                            
-                            explore_dir <= DIR_GET_UPRIGHT;
                         end
                         
                         DIR_GET_UPRIGHT: begin
-                            if (addr - 640 + 1 == addr_prev) begin
+                            if (addr_curr - 640 + 1 == addr_prev) begin
                                 state <= STATE_EXPLORE;
+                                explore_dir <= explore_dir + 1;
                             end else begin
-                                x_explore <= x_curr + 1;
-                                y_explore <= y_curr - 1;
-                                addr_explore <= addr - 640 + 1;
+                                addr <= addr_curr - 640 + 1;
                             end
-                            
-                            explore_dir <= DIR_GET_RIGHT;
                         end
                     endcase                
                 end
                 
                 STATE_IS_IT_EDGE: begin
-//                    if (addr_explore == addr_start) begin
-//                        done <= 1;
-                    if (set_bin == 3) begin
-                        done <= 1;
+                    state <= STATE_EXPLORE;
+
+                    if (addr == addr_start) begin
+                        state <= STATE_DONE;
                     end
                     else begin
                         if (edge_out != 3'b000) begin
                             //update our memory of current and past pixels
-                            x_prev <= x_curr;
-                            y_prev <= y_curr;
-                            addr_prev <= addr_curr;
-                            
-                            x_curr <= x_explore;
-                            y_curr <= y_explore;
-                            addr_curr <= addr_explore;
+                            addr_prev <= addr_curr;                            
+                            addr_curr <= addr;
                             
                             explore_dir <= 0;
+                            explore_dir_count <= 0;
                             pixel_count <= pixel_count + 1;
                             
                             //write that bin into the bram location. check to see if we have all pixels in that bin
-                            addr <= addr_explore;
                             we <= 1;
-                            bin_in <= set_bin;
                             if (pixel_count == pixel_per_bin) begin
                                 pixel_count <= 0;
-                                set_bin <= set_bin + 1;
-                            end                                   
+                                bin_in <= bin_in + 1;
+                            end
                         end
                         else begin
-                            state <= STATE_EXPLORE;
                             explore_dir <= explore_dir + 1;
-                            if (explore_dir == max_explore_dir) begin
-                                done <= 1;
+                            explore_dir_count <= explore_dir_count + 1;
+                            if (explore_dir_count == max_explore_dir) begin
+                                state <= STATE_DONE;
                             end
                         end
                     end
                 end
-            
+                
+                STATE_DONE: begin
+                    done <= 1;
+                    we <= 0;
+                end            
             
             endcase
         end
-    end
-    
+    end    
 endmodule
